860df30b95734ddc2c678866638c352b83625cd8
emmyni <emmyni220@gmail.com>
2020-04-30 19:59:32 -0400

    Start the generate meal plan form

---
+++ meal_planner/frontend/src/components/mealplan/Form.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { addPantry } from "../../actions/pantry";
+
+export class Form extends Component {
+  state = {
+    diet: "",
+    exclude: "",
+    targetCalories: 0,
+    timeFrame: "day",
+  };
+
+  static propTypes = {
+    addPantry: PropTypes.func.isRequired,
+  };
+
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const { diet, exclude, targetCalories, timeFrame } = this.state;
+    const item = { diet, exclude, targetCalories, timeFrame };
+    this.props.addPantry(item);
+    this.setState({
+      diet: "",
+      exclude: "",
+      targetCalories: 0,
+      timeFrame: "day",
+    });
+  };
+  render() {
+    const { diet, exclude, targetCalories, timeFrame } = this.state;
+    return (
+      <div className="card card-body mt-4 mb-4">
+        <h2>Generate a Meal Plan</h2>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Diet</label>
+            <input
+              className="form-control"
+              type="text"
+              name="diet"
+              onChange={this.onChange}
+              value={diet}
+            />
+          </div>
+          <div className="form-group">
+            <label>Exclude</label>
+            <input
+              className="form-control"
+              type="text"
+              name="exclude"
+              onChange={this.onChange}
+              value={exclude}
+            />
+          </div>
+          <div className="form-group">
+            <label>Target Calories</label>
+            <input
+              className="form-control"
+              type="number"
+              name="targetCalories"
+              onChange={this.onChange}
+              value={targetCalories}
+            />
+          </div>
+          <div className="form-group">
+            <label>Time Frame</label>
+            <input
+              className="form-control"
+              type="text"
+              name="timeFrame"
+              onChange={this.onChange}
+              value={timeFrame}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default connect(null, { addPantry })(Form);
--- meal_planner/frontend/src/components/mealplan/index.js
+++ meal_planner/frontend/src/components/mealplan/index.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react";
 import Sample from "./Sample";
+import Form from "./Form";
 
 export default function GeneratePlan() {
   return (
     <Fragment>
+      <Form />
       <Sample />
     </Fragment>
   );
